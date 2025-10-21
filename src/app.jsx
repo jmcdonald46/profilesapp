db9230343bff38aa939d288c185da616f0a32658
jmcdonald46 <jmcdonald46@my.gcu.edu>
2025-10-20 22:48:04 -0700

    'content'

--- src/app.jsx
+++ src/app.jsx
@@ -18,35 +18,35 @@ function App() {
                     <p>With a <b>flexible</b> layout.</p>
                 </header>
 
-                <div class="navbar">
+                <div className="navbar">
                     <a href="#">Link</a>
                     <a href="#">Link</a>
                     <a href="#">Link</a>
                     <a href="#">Link</a>
                 </div>
 
-                <div class="container">
-                    <div class="side">
+                <div className="container">
+                    <div className="side">
                         <h2>About Me</h2>
                         <h5>Photo of me:</h5>
-                        <div class="fakeimg" style="height:200px;">Image</div>
+                        <div className="fakeimg" style="height:200px;">Image</div>
                         <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
                         <h3>More Text</h3>
                         <p>Lorem ipsum dolor sit ame.</p>
-                        <div class="fakeimg" style="height:60px;">Image</div><br />
-                            <div class="fakeimg" style="height:60px;">Image</div><br />
-                                <div class="fakeimg" style="height:60px;">Image</div>
+                        <div className="fakeimg" style="height:60px;">Image</div><br />
+                            <div className="fakeimg" style="height:60px;">Image</div><br />
+                                <div className="fakeimg" style="height:60px;">Image</div>
                             </div>
-                            <div class="main">
+                            <div className="main">
                                 <h2>TITLE HEADING</h2>
                                 <h5>Title description, Oct 7, 2025</h5>
-                                <div class="fakeimg" style="height:200px;">Image</div>
+                                <div className="fakeimg" style="height:200px;">Image</div>
                                 <p>Some text..</p>
                                 <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
                                 <br />
                                     <h2>TITLE HEADING</h2>
                                     <h5>Title description, Sep 2, 2025</h5>
-                                    <div class="fakeimg" style="height:200px;">Image</div>
+                                    <div className="fakeimg" style="height:200px;">Image</div>
                                     <p>Some text..</p>
                                     <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
                             </div>
