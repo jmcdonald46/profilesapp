bb96d1edea9259a8878cc7c34a7b220c633e653c
jmcdonald46 <jmcdonald46@my.gcu.edu>
2026-01-21 16:29:55 -0700

    'updatedpdf'

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    publicDir: 'public', // Ensures public folder is copied to dist
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        copyPublicDir: true // Explicitly copy public directory
+    }
 })
